b916f80d52faee6b90b496b471e154cf6e603a1b
victorwu <victorwu@users.noreply.github.com>
2022-06-16 01:13:37 -0400

    fix: semantic.min.css import error via https://github.com/Semantic-Org/Semantic-UI/issues/7073

--- src/UIElements/popup/index.tsx
+++ src/UIElements/popup/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-// import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
