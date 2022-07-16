da70689a04841f83471b746d1fe2db172d019f86
victorwu <victorwu@users.noreply.github.com>
2022-07-15 21:49:23 -0400

    refactor: encoding code

--- src/serviceworker/database/index.ts
+++ src/serviceworker/database/index.ts
@@ -5,7 +5,9 @@ import createHash from 'create-hash';
 import { encrypt, decrypt, importKey, generateKey } from '@utils/subtleCrypto';
 
 let db: IDBPDatabase<AccountSchema>;
-
+var iv = new Uint8Array([188, 185, 57, 146, 246, 194, 116, 34, 12, 80, 198, 76]);
+var textEnc = new TextEncoder();
+var textDec = new TextDecoder("utf-8");
 
 export const createDB = async (dbName: string = config.name) => {
   db = await openDB<AccountSchema>(dbName, 1, {
@@ -18,20 +20,18 @@ export const createDB = async (dbName: string = config.name) => {
 export const insertAccount = async (account: IAccount) => {
   const accountCount = await db.count('account');
 
-  var keys = await importKey()
-  var iv = new Uint8Array([188, 185, 57, 146, 246, 194, 116, 34, 12, 80, 198, 76]);
-  var enc = new TextEncoder();
+  var keys = await importKey();
 
   var eAccount = {
     password: account.password,
-    privateKey: await encrypt(enc.encode(account.privateKey), keys, iv),
-    privateExtendedKey: await encrypt(enc.encode(account.privateExtendedKey), keys, iv),
+    privateKey: await encrypt(textEnc.encode(account.privateKey), keys, iv),
+    privateExtendedKey: await encrypt(textEnc.encode(account.privateExtendedKey), keys, iv),
     address: [] as Array<ArrayBuffer>
   } as EAccount;
 
 
   for (let i = 0; i < account.address.length; i++) {
-    var data = enc.encode(account.address[i]);
+    var data = textEnc.encode(account.address[i]);
     eAccount.address[i] = await encrypt(data, keys, iv);
   }
 
@@ -42,32 +42,28 @@ export const insertIdentity = async (identity: string, accountId: number = 0) =>
   const account = await db.get('account', accountId) as EAccount;
   // @ts-ignore
 
-  var keys = await importKey()
-  var iv = new Uint8Array([188, 185, 57, 146, 246, 194, 116, 34, 12, 80, 198, 76]);
-  var enc = new TextEncoder();
+  var keys = await importKey();
 
-  var data = enc.encode(identity);
+  var data = textEnc.encode(identity);
   account.address[account.address.length] = await encrypt(data, keys, iv);
   // @ts-ignore
   db.put('account', account, accountId);
 }
 
 export const getAccount = async (accountId: number = 0) => {
-  var keys = await importKey()
-  var iv = new Uint8Array([188, 185, 57, 146, 246, 194, 116, 34, 12, 80, 198, 76]);
-  var enc = new TextDecoder("utf-8");
+  var keys = await importKey();
 
   const account = await db.get('account', accountId) as EAccount;
   const iAccount = {
     password: account.password,
-    privateKey: enc.decode(await decrypt(account.privateKey, keys, iv)),
-    privateExtendedKey: enc.decode(await decrypt(account.privateExtendedKey, keys, iv)),
+    privateKey: textDec.decode(await decrypt(account.privateKey, keys, iv)),
+    privateExtendedKey: textDec.decode(await decrypt(account.privateExtendedKey, keys, iv)),
     address: [] as Array<string>
   } as IAccount;
 
   for (let i = 0; i < account.address.length; i++) {
     var decryptedData = await decrypt(account.address[i], keys, iv);
-    iAccount.address[i] = enc.decode(decryptedData);
+    iAccount.address[i] = textDec.decode(decryptedData);
   }
 
   return iAccount;
@@ -84,12 +80,10 @@ export const retrievePrivateKey = async (accountId: number = 0) => {
   const res = await db.get('account', accountId) as EAccount;
   // @ts-ignore
 
-  var keys = await importKey()
-  var iv = new Uint8Array([188, 185, 57, 146, 246, 194, 116, 34, 12, 80, 198, 76]);
-  var enc = new TextDecoder("utf-8");
+  var keys = await importKey();
 
   var decryptedData = await decrypt(res.privateKey, keys, iv);
-  return enc.decode(decryptedData);
+  return textDec.decode(decryptedData);
 };
 
 export const getIdentityCount = async (accountId: number = 0) => {
