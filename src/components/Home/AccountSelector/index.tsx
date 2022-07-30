f1664e656688e762ec8a8e5f65220b4c39ebc2e7
victorwu <victorwu@users.noreply.github.com>
2022-07-29 21:37:48 -0400

    fix: identity selector

--- src/components/Home/AccountSelector/index.tsx
+++ src/components/Home/AccountSelector/index.tsx
@@ -13,20 +13,20 @@ export default () => {
 
 
   const accountOptions = useMemo(() => identities.map((identity, idx) => ({
-    key: identity[0].address,
-    value: identity[0].address,
+    key: idx,
+    value: idx,
     text: `Identity ${idx} (${cutter(identity[0].address)})`
   })), [identities]);
 
   const handleSelectAccount = useCallback((event: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
-    dispatch(selectIdentity(data.value as string));
+    dispatch(selectIdentity(data.value as number));
   }, []);
 
   return (
     <Dropdown
       selection
       options={accountOptions}
-      value={(identities.length && identities[selectedIdentityId][0].address) || accountOptions[0]?.value}
+      value={(selectedIdentityId) || 0}
       onChange={handleSelectAccount} />
   );
 }
