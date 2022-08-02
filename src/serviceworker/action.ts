4f297f582907d6845caeea8f6b3f0fab0ea6b952
victorwu <victorwu@users.noreply.github.com>
2022-08-02 18:02:03 -0400

    fix: extension reload bug - integrate getAccountValid function

--- src/serviceworker/action.ts
+++ src/serviceworker/action.ts
@@ -1,4 +1,5 @@
 import { syncGet } from '../storage';
+import { getAccountValid } from './database';
 
 export const handleActionClick = async () => {
   chrome.tabs.create({
@@ -8,8 +9,12 @@ export const handleActionClick = async () => {
 
 export const handleStartup = async () => {
   const accountStatus = await syncGet('accountStatus');
+  const accValid = await getAccountValid();
 
-  if (accountStatus !== undefined) {
+  if (accValid === true)
+    chrome.action.setPopup({ popup: 'index.html?popup=true' });
+
+  if (typeof accountStatus == 'string' && accountStatus == 'set') {
     chrome.action.setPopup({ popup: 'index.html?popup=true' });
   }
 }
