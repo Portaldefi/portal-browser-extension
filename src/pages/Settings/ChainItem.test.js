7529a64bc6e32ee329b78a5dbe75893077d89ffd
victorwu <victorwu@users.noreply.github.com>
2022-10-26 09:38:26 -0700

    feature: add semantic to chainitem

--- src/pages/Settings/ChainItem.test.js
+++ src/pages/Settings/ChainItem.test.js
@@ -1,6 +1,7 @@
 import ChainItem from './ChainItem';
 import { create, act } from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
+import { Checkbox, Grid, Header } from 'semantic-ui-react';
 
 describe('ChainItemComponent', () => {
   it('should work', async () => {
