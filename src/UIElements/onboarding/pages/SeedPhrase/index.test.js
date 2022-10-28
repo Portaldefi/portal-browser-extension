db12b164c2f707ef6972b34e9b067bf8a1874a87
victorwu <victorwu@users.noreply.github.com>
2022-10-28 11:58:03 -0700

    feature: add test for seed phrase page

--- src/UIElements/onboarding/pages/SeedPhrase/index.test.js
+++ src/UIElements/onboarding/pages/SeedPhrase/index.test.js
@@ -3,26 +3,28 @@ import { create, act } from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store'
+import { Button } from 'semantic-ui-react';
 
 describe('SeedPhraseComponent', () => {
-  const initialState = {
-    phrase: {
-      SRF_Length: 0,
-      SRF_List: [],
-      password: ''
-    },
-    menu: {
-      items: []
+  let initialState, mockStore, store;
+  beforeEach(() => {
+    initialState = {
+      phrase: {
+        SRF_Length: 10,
+        SRF_List: ["123", "asdf"],
+        password: ''
+      },
+      menu: {
+        items: []
+      }
     }
-  }
-  const mockStore = configureStore()
-  let store
-
+    mockStore = configureStore()
+    store = mockStore(initialState)
+  });
 
   it('should work', async () => {
     let tree;
     act(() => {
-      store = mockStore(initialState)
       tree = create(
         <Provider store={store}>
           <BrowserRouter>
@@ -32,6 +34,21 @@ describe('SeedPhraseComponent', () => {
       );
     });
 
+    expect(tree).toMatchSnapshot();
+  });
+  it('functions', async () => {
+    let tree;
+    act(() => {
+      tree = create(
+        <Provider store={store}>
+          <BrowserRouter>
+            <SeedPhrase />
+          </BrowserRouter>
+        </Provider>
+      );
+    });
+    const buttons = tree.root.findAllByType(Button);
+    act(() => buttons[0].props.onClick());
     expect(tree).toMatchSnapshot();
   });
   afterAll(() => jest.resetModules());
