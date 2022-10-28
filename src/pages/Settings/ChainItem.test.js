50b04226b5be17ed2ee449ae179540771beb3c2a
victorwu <victorwu@users.noreply.github.com>
2022-10-27 17:00:38 -0700

    feature: chainItem test check state

--- src/pages/Settings/ChainItem.test.js
+++ src/pages/Settings/ChainItem.test.js
@@ -1,20 +1,49 @@
 import ChainItem from './ChainItem';
 import { create, act } from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
-import { Checkbox, Grid, Header } from 'semantic-ui-react';
+import { Checkbox } from 'semantic-ui-react';
 
-describe('ChainItemComponent', () => {
+describe('ChainItem', () => {
+  let handleSetCheckState, index;
+  beforeEach(() => {
+    handleSetCheckState = jest.fn();
+    index = 0;
+  });
   it('should work', async () => {
     let tree;
     act(() => {
       tree = create(
         <BrowserRouter>
-          <ChainItem />
+          <ChainItem
+            handleSetCheckState={handleSetCheckState}
+            index={index} />
         </BrowserRouter>
       );
     });
-
     expect(tree).toMatchSnapshot();
+  });
+  it('checkbox should work', () => {
+    let tree;
+    act(() => {
+      tree = create(
+        <BrowserRouter>
+          <ChainItem
+            handleSetCheckState={handleSetCheckState}
+            index={index} />
+        </BrowserRouter>
+      );
+    });
+    const item = tree.root.findByType(Checkbox);
+    act(() => item.props.onChange({
+      target: {
+        value: true
+      }
+    }));
+    expect(tree).toMatchSnapshot();
+    expect(handleSetCheckState).toHaveBeenCalledWith(
+      index,
+      true
+    );
   });
   afterAll(() => jest.resetModules());
 });
