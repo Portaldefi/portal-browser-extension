c9e02abc9bdd3f59615c6bbdb9456b6853d6965e
victorwu <victorwu@users.noreply.github.com>
2022-10-27 17:01:18 -0700

    feature: GlobalChainItem test

--- src/pages/Settings/GlobalChainItem.test.js
+++ src/pages/Settings/GlobalChainItem.test.js
@@ -1,19 +1,49 @@
 import GlobalChainItem from './GlobalChainItem';
 import { create, act } from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
+import { Checkbox } from 'semantic-ui-react';
 
 describe('GlobalChainItemComponent', () => {
+  let handleSetCheckState, index;
+  beforeEach(() => {
+    handleSetCheckState = jest.fn();
+    index = 0;
+  });
   it('should work', async () => {
     let tree;
     act(() => {
       tree = create(
         <BrowserRouter>
-          <GlobalChainItem />
+          <GlobalChainItem
+            handleSetCheckState={handleSetCheckState}
+            index={index} />
         </BrowserRouter>
       );
     });
-
     expect(tree).toMatchSnapshot();
+  });
+  it('checkbox should work', () => {
+    let tree;
+    act(() => {
+      tree = create(
+        <BrowserRouter>
+          <GlobalChainItem
+            handleSetCheckState={handleSetCheckState}
+            index={index} />
+        </BrowserRouter>
+      );
+    });
+    const item = tree.root.findByType(Checkbox);
+    act(() => item.props.onChange({
+      target: {
+        value: true
+      }
+    }));
+    expect(tree).toMatchSnapshot();
+    expect(handleSetCheckState).toHaveBeenCalledWith(
+      index,
+      true
+    );
   });
   afterAll(() => jest.resetModules());
 });
