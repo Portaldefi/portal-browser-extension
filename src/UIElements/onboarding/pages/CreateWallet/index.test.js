2fd247f73dec5e42dfe860f4983ce9dc06e71599
victorwu <victorwu@users.noreply.github.com>
2022-10-31 16:24:07 -0700

    feature: add tests to createWallet

--- src/UIElements/onboarding/pages/CreateWallet/index.test.js
+++ src/UIElements/onboarding/pages/CreateWallet/index.test.js
@@ -3,26 +3,30 @@ import { create, act } from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store'
+import { Button, Form } from 'semantic-ui-react';
 
 describe('CreateWalletComponent', () => {
-  const initialState = {
-    phrase: {
-      SRF_Length: 0,
-      SRF_List: [],
-      password: ''
-    },
-    menu: {
-      items: []
+  let initialState;
+  let mockStore;
+  let store;
+  beforeEach(() => {
+    initialState = {
+      phrase: {
+        SRF_Length: 0,
+        SRF_List: [],
+        password: ''
+      },
+      menu: {
+        items: []
+      }
     }
-  }
-  const mockStore = configureStore()
-  let store
-
+    mockStore = configureStore()
+    store = mockStore(initialState);
+  });
 
   it('should work', async () => {
     let tree;
     act(() => {
-      store = mockStore(initialState)
       tree = create(
         <Provider store={store}>
           <BrowserRouter>
@@ -32,6 +36,33 @@ describe('CreateWalletComponent', () => {
       );
     });
 
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('functions', async () => {
+    let tree;
+    act(() => {
+      tree = create(
+        <Provider store={store}>
+          <BrowserRouter>
+            <CreateWallet />
+          </BrowserRouter>
+        </Provider>
+      );
+    });
+
+    const buttons = tree.root.findAllByType(Button);
+    act(() => buttons[0].props.onClick());
+    expect(tree).toMatchSnapshot();
+    act(() => buttons[1].props.onClick());
+    expect(tree).toMatchSnapshot();
+
+    const inputs = tree.root.findAllByType(Form.Input);
+    act(() => inputs[0].props.onChange({
+      target: {
+        value: "1123"
+      }
+    }));
     expect(tree).toMatchSnapshot();
   });
   afterAll(() => jest.resetModules());
