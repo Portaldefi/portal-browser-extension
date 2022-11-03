f514e1846faf4eeecaf02e92d5dbb6a027d73615
victorwu <victorwu@users.noreply.github.com>
2022-11-02 17:24:26 -0700

    feature: test select action

--- src/UIElements/onboarding/pages/SelectAction/index.test.js
+++ src/UIElements/onboarding/pages/SelectAction/index.test.js
@@ -1,6 +1,7 @@
 import SelectAction from './index';
 import { create, act } from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 
 describe('SelectActionComponent', () => {
   it('should work', () => {
@@ -13,6 +14,21 @@ describe('SelectActionComponent', () => {
       );
     });
 
+    expect(tree).toMatchSnapshot();
+  });
+  it('button clicks', () => {
+    let tree;
+    act(() => {
+      tree = create(
+        <BrowserRouter>
+            <SelectAction />
+        </BrowserRouter>,
+      );
+    });
+
+    const buttons = tree.root.findAllByType(Button);
+    act(() => buttons[0].props.onClick());
+    act(() => buttons[1].props.onClick());
     expect(tree).toMatchSnapshot();
   });
   afterAll(() => jest.resetModules());
