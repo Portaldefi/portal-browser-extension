65d73b3be2ef06726dae87690d45fcfcda578bfc
victorwu <victorwu@users.noreply.github.com>
2022-11-30 00:22:02 -0400

    style: transaction format

--- src/components/Home/TransactionHistory/TransactionItem.tsx
+++ src/components/Home/TransactionHistory/TransactionItem.tsx
@@ -5,12 +5,21 @@ interface ITransactionItem {
   tx: any
 };
 
+const shortenString = (str: string) => {
+  return str.substring(0, 6) + '...' + str.substring(str.length - 3);
+};
+
+const dateToString = (dat: Date) => {
+  return dat.getFullYear() + '-' + (dat.getMonth() + 1) + '-' + dat.getDate();
+}
+
 export default ({tx}: ITransactionItem) => {
   console.log(tx);
   return (
     <Grid className='p-16 py-9 bg-white'>
       <Grid.Column width='eleven' textAlign='left'>
-        <Header as='p' className='description'>{tx.vin[0].prevout.value}</Header>
+        <Header as='p' className='description'>{shortenString(tx.txid)}</Header>
+        <Header as='p' className='description'>{dateToString(new Date(tx.status.block_time))}</Header>
       </Grid.Column>
       <Grid.Column width='five' verticalAlign='middle'>
         {tx.vout.map((row: any) => <Header as='p' className='description'>{row.value}</Header>)}
