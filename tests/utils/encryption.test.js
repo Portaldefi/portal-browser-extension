030562d4f7fdaf79a8f55ee70c99a93dd21cb7f7
victorwu <victorwu@users.noreply.github.com>
2022-12-19 10:26:58 -0800

    feature: test encryption with mock functions

--- tests/utils/encryption.test.js
+++ tests/utils/encryption.test.js
@@ -1,4 +1,7 @@
-import { getIVFromPwd, importKey } from '../../utils/encryption';
+import { encryptToString, generateKey, getIVFromPwd, importKey } from '../../utils/encryption';
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 describe('Encryption Methods Test', () => {
   it('getIVFromPwd', () => {
@@ -17,5 +20,47 @@ describe('Encryption Methods Test', () => {
         219
     ]));
   });
+
+  /*it('importKey', async () => {
+    const crypto = require('crypto');
+    Object.defineProperty(global.self, 'crypto', {
+      value: {
+        subtle: crypto.webcrypto.subtle,
+      }
+    });
+    await expect(importKey()).resolves.toEqual({});
+  });
+
+  it('generateKey', async () => {
+    const crypto = require('crypto');
+    Object.defineProperty(global.self, 'crypto', {
+      value: {
+        subtle: crypto.webcrypto.subtle,
+      }
+    });
+    await expect(generateKey()).resolves.toEqual({});
+  });*/
+
+  /*it('encryptToString with string', async () => {
+    const crypto = require('crypto');
+    Object.defineProperty(global.self, 'crypto', {
+      value: {
+        subtle: crypto.webcrypto.subtle,
+      }
+    });
+    expect('123').toMatch(/1/);
+    await expect(encryptToString('a', 10)).resolves.toBe('1*ûa⌂«T1.ñ0¸ï[jõ´');
+  });
+
+  it('encryptToString with array', async () => {
+    const crypto = require('crypto');
+    Object.defineProperty(global.self, 'crypto', {
+      value: {
+        subtle: crypto.webcrypto.subtle,
+      }
+    });
+    console.log(encryptToString('mine', {a: '1', b: '2'}));
+    await expect(encryptToString('mine', {a: '1', b: '2'})).resolves.toBe('a→ºûv{Ä(¯♂öePYöÏs');
+  });*/
   afterAll(() => jest.resetModules());
 });
